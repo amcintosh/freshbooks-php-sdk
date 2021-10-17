37589a34cd621897879ccb5405a971ac22d11e98
Andrew McIntosh <amcintosh@freshbooks.com>
2021-10-17 09:14:01 -0400

    ✨ Ability to make a basic call

--- src/Client.php
+++ src/Client.php
@@ -4,8 +4,24 @@ declare(strict_types=1);
 
 namespace amcintosh\FreshBooks;
 
+use amcintosh\FreshBooks\api\Accounting;
+use Http\Client\Common\HttpMethodsClient;
+use Http\Client\Common\Plugin;
+use Http\Client\Common\Plugin\BaseUriPlugin;
+use Http\Client\Common\Plugin\HeaderDefaultsPlugin;
+use Http\Client\Common\PluginClientFactory;
+use Http\Discovery\HttpClientDiscovery;
+use Http\Discovery\Psr17FactoryDiscovery;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+
 class Client
 {
+
+    private ClientInterface $httpClient;
+    private RequestFactoryInterface $requestFactoryInterface;
+    private StreamFactoryInterface $streamFactoryInterface;
 
     private ClientConfig $config;
 
@@ -13,12 +29,46 @@ class Client
     {
         $this->config = $config;
         $this->config->clientId = $clientId;
+        $this->httpClient = $this->createHttpClient();
     }
 
-    public function printConfig()
+    private function getHeaders(): array
     {
-        var_dump($this->config->clientId);
-        var_dump($this->config->clientSecret);
-        var_dump($this->config->getUserAgent());
+        return [
+            'Authorization' => 'Bearer ' . $this->config->accessToken,
+            'User-Agent' => $this->config->getUserAgent(),
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ];
+    }
+
+    private function createHttpClient()
+    {
+        $plugins = array(
+            new BaseUriPlugin(Psr17FactoryDiscovery::findUriFactory()->createUri('https://api.freshbooks.com')),
+            new HeaderDefaultsPlugin($this->getHeaders()),
+        );
+
+        $pluginClient = (new PluginClientFactory())->createClient(
+            HttpClientDiscovery::find(),
+            $plugins
+        );
+
+        return new HttpMethodsClient(
+            $pluginClient,
+            Psr17FactoryDiscovery::findRequestFactory(),
+            Psr17FactoryDiscovery::findStreamFactory()
+        );
+    }
+
+
+    /**
+     * FreshBooks clients resource with calls to get, list, create, update, delete
+     *
+     * @return array
+     */
+    public function clients(): Accounting
+    {
+        return new Accounting($this->httpClient);
     }
 }
