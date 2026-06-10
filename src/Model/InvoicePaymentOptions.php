907d565b4464d3f2ee7be87cc3aee3a415aa60a9
Andrew McIntosh <nacho.vonkickbutt@gmail.com>
2026-06-09 20:55:10 -0400

    Spatie removal from invoice payment options
    Continuing the Spatie removal.
    
    Closes #97

--- src/Model/InvoicePaymentOptions.php
+++ src/Model/InvoicePaymentOptions.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace amcintosh\FreshBooks\Model;
 
-use Spatie\DataTransferObject\Attributes\MapFrom;
-use Spatie\DataTransferObject\Attributes\MapTo;
-use Spatie\DataTransferObject\Caster;
-use Spatie\DataTransferObject\DataTransferObject;
 use amcintosh\FreshBooks\Model\DataModel;
+use amcintosh\FreshBooks\Util;
 
 /**
  * In FreshBooks, invoices can be paid online via a variety of payment gateways
@@ -22,7 +19,7 @@ use amcintosh\FreshBooks\Model\DataModel;
  * @package amcintosh\FreshBooks\Model
  * @link https://www.freshbooks.com/api/online-payments
  */
-class InvoicePaymentOptions extends DataTransferObject implements DataModel
+class InvoicePaymentOptions implements DataModel
 {
     public const RESPONSE_FIELD = 'payment_options';
 
@@ -31,57 +28,55 @@ class InvoicePaymentOptions extends DataTransferObject implements DataModel
      *
      * _Note_: The API returns this as `entity_id`.
      */
-    #[MapFrom('entity_id')]
-    #[MapTo('entity_id')]
     public ?string $entityId;
 
     /**
      * @var string Eg. “invoices”.
      */
-    #[MapFrom('entity_type')]
-    #[MapTo('entity_type')]
     public ?string $entityType;
 
     /**
      * @var string Payment gateway name.
      */
-    #[MapFrom('gateway_name')]
-    #[MapTo('gateway_name')]
     public ?string $gatewayName;
 
     /**
      * @var bool If the invoice can accept credit cards.
      */
-    #[MapTo('has_credit_card')]
-    #[MapFrom('has_credit_card')]
     public ?bool $hasCreditCard;
 
     /**
      * @var bool If the invoice can accept ACH bank transfers.
      */
-    #[MapTo('has_ach_transfer')]
-    #[MapFrom('has_ach_transfer')]
     public ?bool $hasAchTransfer;
 
-    #[MapFrom('has_acss_debit')]
     public ?bool $hasAcssDebit;
 
-    #[MapFrom('has_bacs_debit')]
     public ?bool $hasBacsDebit;
 
-    #[MapFrom('has_sepa_debit')]
     public ?bool $hasSepaDebit;
 
-    #[MapFrom('has_paypal_smart_checkout')]
     public ?bool $hasPaypalSmartCheckout;
 
     /**
      * @var bool If the client can use the gateway to pay part
      * of the invoice or only the full amount.
      */
-    #[MapTo('allow_partial_payments')]
-    #[MapFrom('allow_partial_payments')]
     public ?bool $allowPartialPayments;
+
+    public function __construct(array $data = [])
+    {
+        $this->entityId = isset($data['entity_id']) ? (string) $data['entity_id'] : null;
+        $this->entityType = $data['entity_type'] ?? null;
+        $this->gatewayName = $data['gateway_name'] ?? null;
+        $this->hasCreditCard = $data['has_credit_card'] ?? null;
+        $this->hasAchTransfer = $data['has_ach_transfer'] ?? null;
+        $this->hasAcssDebit = $data['has_acss_debit'] ?? null;
+        $this->hasBacsDebit = $data['has_bacs_debit'] ?? null;
+        $this->hasSepaDebit = $data['has_sepa_debit'] ?? null;
+        $this->hasPaypalSmartCheckout = $data['has_paypal_smart_checkout'] ?? null;
+        $this->allowPartialPayments = $data['allow_partial_payments'] ?? null;
+    }
 
     /**
      * Get the data as an array to POST or PUT to FreshBooks, removing any read-only fields.
@@ -90,18 +85,13 @@ class InvoicePaymentOptions extends DataTransferObject implements DataModel
      */
     public function getContent(): array
     {
-        $data = $this
-            ->except('id')
-            ->except('hasAcssDebit')
-            ->except('hasBacsDebit')
-            ->except('hasSepaDebit')
-            ->except('hasPaypalSmartCheckout')
-            ->toArray();
-        foreach ($data as $key => $value) {
-            if (is_null($value)) {
-                unset($data[$key]);
-            }
-        }
+        $data = array();
+        Util::convertContent($data, 'entity_id', $this->entityId);
+        Util::convertContent($data, 'entity_type', $this->entityType);
+        Util::convertContent($data, 'gateway_name', $this->gatewayName);
+        Util::convertContent($data, 'has_credit_card', $this->hasCreditCard);
+        Util::convertContent($data, 'has_ach_transfer', $this->hasAchTransfer);
+        Util::convertContent($data, 'allow_partial_payments', $this->allowPartialPayments);
         return $data;
     }
 }
